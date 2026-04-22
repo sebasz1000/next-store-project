b4e3bb1ca41b1da80484ac7ee7d95e4042facc6f
Sebastián Zapata <sebasz1000@gmail.com>
2026-04-22 15:11:12 -0500

    feat: corrects fetch product url and properly displays product category

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -6,12 +6,13 @@ interface ProductPageProps{
 export default async function ProductPage({ params }:ProductPageProps){
 
     const {id} = await params
-    const response = await fetch(`https://dummyjson.com/productss/1`)
+    const response = await fetch(`https://dummyjson.com/products/${id}`)
     const {title, description, thumbnail, category}: Product = await response.json()
-    console.log(id)
+    const uppercasedCategory = category.charAt(0).toUpperCase() + category.slice(1)
     return (<>
     <img src={thumbnail} alt={title} className="w-80" />
         <h1 className="text-3xl">{title}</h1>
+        <span className="text-2xl">{uppercasedCategory}</span>
         <p>{description}</p>
     </>)
 }
