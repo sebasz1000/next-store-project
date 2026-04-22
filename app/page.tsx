1b269e749ccf9ef11d1eb05f7c39985e1eefa22d
Sebastián Zapata <sebasz1000@gmail.com>
2026-04-22 16:32:18 -0500

    feat: add layout for product page in order to implement more features like related products(sidebar) in the future

--- app/page.tsx
+++ app/page.tsx
@@ -16,15 +16,13 @@ export default async function Home({ searchParams}: HomeProps) {
   const data: APIResponse = await response.json()
   const {products} = data 
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-transparent">
         
-       <h1 className="text-3xl mb-10">STORE!!</h1>
+       <h1 className="text-3xl mb-10 mt-8">Explore our products</h1>
           <Suspense fallback={<div>Loading Searcher....</div>}>
             <Search />
           </Suspense>
           <List products={products} />
-      </main>
     </div>
   );
 }
