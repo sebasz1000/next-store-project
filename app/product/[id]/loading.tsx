ab64698e7ecff08e2cf0b5a0f70fd0daa86fa2e9
Sebastián Zapata <sebasz1000@gmail.com>
2026-04-22 14:29:34 -0500

    feat:add loading state for product detail

---
+++ app/product/[id]/loading.tsx
@@ -0,0 +1,7 @@
+export default async function Loading(){
+    return (
+    <div className="flex items-center justify-center min-h-screen">
+      <p className="text-2xl font-bold animate-pulse">Cargando producto...</p>
+    </div>
+  )
+}
